dd237bd86b85f0c264f36ebea21fe41196569f61
Harsh <harshitachavva9@gmail.com>
2026-08-28 14:41:32 +0530

    Frontend and backend build  validated

--- src/app/admin/reports/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import AdminDashboard from '../page';
-
-export default function AdminReportsPage() {
-  return <AdminDashboard initialTab="reports" />;
-}
--- src/app/admin/workers/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import AdminDashboard from '../page';
-
-export default function AdminWorkersPage() {
-  return <AdminDashboard initialTab="workers" />;
-}
